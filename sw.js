6d10386b8120c79217eaa1a605e66a78c70d9acd
kerojjj777-art <kerojjj777@gmail.com>
2026-07-07 00:21:00 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'onion-store-v0.4.1.7';
+const CACHE_NAME = 'onion-store-v0.4.1.8';
 
 const ASSETS = [
     './',
-    './index.html?v=0.4.1.7',
-    './app.js?v=0.4.1.7',
+    './index.html?v=0.4.1.8',
+    './app.js?v=0.4.1.8',
     './manifest.json',
     './icon-192.png',
     './icon-512.png',
